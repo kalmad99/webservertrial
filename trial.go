86606d9c6d52b91f2f7f97e2ecbaf0c4182eb6aa
Kaleb Mesfin <kalemesfin12@gmail.com>
2019-10-24 22:39:37 +0300

    added html template

--- trial.go
+++ trial.go
@@ -8,7 +8,8 @@ import (
 var templ = template.Must(template.ParseFiles("index.html"))
 
 func index(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Kaleb Mesfin</h1><p>I am not sick of programming</p>"))
+	// w.Write([]byte("<h1>Kaleb Mesfin</h1><p>I am not sick of programming</p>"))
+	templ.Execute(w, nil)
 }
 
 func main() {

51158b695e293178ffb8fad24e0fc38ef928cd67
Kaleb Mesfin <kalemesfin12@gmail.com>
2019-10-25 00:50:10 +0300

    with a struct

--- trial.go
+++ trial.go
@@ -5,11 +5,18 @@ import (
 	"net/http"
 )
 
+type Course struct {
+	Title, Code, Description string
+	ECTS                     int
+}
+
 var templ = template.Must(template.ParseFiles("index.html"))
 
 func index(w http.ResponseWriter, r *http.Request) {
 	// w.Write([]byte("<h1>Kaleb Mesfin</h1><p>I am not sick of programming</p>"))
-	templ.Execute(w, nil)
+	// templ.Execute(w, nil)
+	course := Course{"DLD", "ITSE-3182", "Lorem Ipsum", 7}
+	templ.Execute(w, course)
 }
 
 func main() {
